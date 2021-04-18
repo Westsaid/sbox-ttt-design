b1b0dc0e30000401178c22ce65e5665598533aa6
Said Adibi <saidadibi4@gmail.com>
2021-04-18 17:05:57 +0200

    Small fixes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 let activeItem
 document.querySelectorAll(".shop .item").forEach(item => {
     item.onclick = (e) => {
-        const eItem = e.target.closest(".item")
+        const eItem = e.target.closest(".item-holder")
         if (eItem.className.includes("active")) return
-        if(activeItem) activeItem.className = 'item'
+        if(activeItem) activeItem.className = 'item-holder'
         eItem.classList.add("active")
         activeItem = eItem
     }
